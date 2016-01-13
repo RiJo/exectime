a76cd7bb1509a821c4eb2d784b4aadb526964fb3
RiJo <rajoo@mail.com>
2016-01-13 19:30:21 +0100

    Templated statistics_t struct + added selector callback for data set

--- src/statistics.hpp
+++ src/statistics.hpp
@@ -1,14 +1,17 @@
 #ifndef __STATISTICS_HPP_INCLUDED__
 #define __STATISTICS_HPP_INCLUDED__
 
+#include <functional>
 #include <algorithm>
 #include <cmath>
 
 namespace statistics {
+    template<typename T>
     struct statistics_t {
         unsigned int items {0};
-        unsigned long maximum {0};
-        unsigned long minimum {0};
+        T maximum {0};
+        T minimum {0};
+        T range {0};
         double average {0.0}; // mean
         double median {0.0};
         double variance {0.0};
@@ -17,36 +20,40 @@ namespace statistics {
     };
 
     // TODO: static assert (list + numeric items)
-    template<typename T>
-    statistics_t calculate(T data_set) {
-        statistics_t s;
+    template<typename TContainer, typename TItem, typename TValue>
+    statistics_t<TValue> calculate(const TContainer &data_set, std::function<TValue (const TItem &)> selector) {
+        statistics_t<TValue> s;
 
         s.items = data_set.size();
         if (s.items == 0)
             return s;
 
-        // Preprocess data set
-        std::sort(data_set.begin(), data_set.end());
+        // Convert data set
+        std::vector<TValue> values;
+        for (const TItem &item: data_set)
+            values.push_back(selector(item));
+        std::sort(values.begin(), values.end());
 
         // Calculate statistics
         if (s.items  % 2 == 0)
-            s.median = (data_set[s.items / 2 - 1] + data_set[s.items / 2]) / 2.0;
+            s.median = (values[s.items / 2 - 1] + values[s.items / 2]) / 2.0;
         else
-            s.median = data_set[s.items / 2];
+            s.median = values[s.items / 2];
 
         s.maximum = std::numeric_limits<unsigned long>::min();
         s.minimum = std::numeric_limits<unsigned long>::max();
-        for (const auto &item: data_set) {
-            if (item < s.minimum)
-                s.minimum = item;
-            if (item > s.maximum)
-                s.maximum = item;
-            s.average += item;
+        for (const TValue &value: values) {
+            if (value < s.minimum)
+                s.minimum = value;
+            if (value > s.maximum)
+                s.maximum = value;
+            s.average += value;
         }
         s.average /= s.items;
+        s.range = s.maximum - s.minimum;
 
-        for (const auto &item: data_set) {
-            s.variance += std::pow(item - s.average, 2);
+        for (const TValue &value: values) {
+            s.variance += std::pow(value - s.average, 2);
         }
         s.variance /= s.items;
 
